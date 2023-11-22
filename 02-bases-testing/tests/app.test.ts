56ea4bce596f4197457230e765c5daae09f963c0
bryananariba <saariel115@gmail.com>
2023-11-22 06:00:41 -0600

    Iniciamdo en el testing

---
+++ 02-bases-testing/tests/app.test.ts
@@ -0,0 +1,17 @@
+describe('App', () => {
+  test('Should be 30', () => {
+
+    // TODO: poner atencion aqui estan los tres AAA de un test
+
+    // Arrange: Arreglar
+    const numUno = 10;
+    const numDos = 20;
+
+    // Act: Actuar
+    const result = numUno + numDos;
+
+    // Assert: Afirmar
+    expect(result).toBe(30);
+
+  })
+});
--- README.md
+++
@@ -1,2 +0,0 @@
-# node-clean-architecture
-0 to Expert en Domain Driven Design
